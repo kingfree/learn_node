9af1b0adc4cb0f394501620eed582bbe2d3bf7c5
Kingfree <anytjf@gmail.com>
2016-09-08 17:16:35 +0800

    选择菜单

--- finder/index.js
+++ finder/index.js
@@ -1,5 +1,29 @@
 const fs = require('fs');
 
-fs.readdir(__dirname, function (err, files) {
-    console.log(files);
+fs.readdir(process.cwd(), function (err, files) {
+    if (!files.length) {
+        return console.log('    \033[31m 当前目录下没有文件\033[39m');
+    }
+    console.log('选择文件或目录:');
+    function file(i) {
+        var filename = files[i];
+        fs.stat(__dirname + '/' + filename, function (err, stat) {
+            if (stat.isDirectory()) {
+                console.log('    ' + i + '    \033[36m' + filename + '/\033[39m');
+            } else {
+                console.log('    ' + i + '    \033[90m' + filename + '\033[39m');
+            }
+            i++;
+            if (i == files.length) {
+                console.log('');
+                process.stdout.write('    \033[33m选择:   \033[39m');
+                process.stdin.resume();
+                process.stdin.setEncoding('utf8');
+            } else {
+                file(i);
+            }
+        });
+    }
+
+    file(0);
 });
