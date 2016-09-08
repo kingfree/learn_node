2e7da6eb99775fc849fcc7da478cc32737812f20
Kingfree <anytjf@gmail.com>
2016-09-08 17:21:45 +0800

    选择重试

--- finder/index.js
+++ finder/index.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
+var stdin = process.stdin;
+var stdout = process.stdout;
 
 fs.readdir(process.cwd(), function (err, files) {
     if (!files.length) {
         return console.log('    \033[31m 当前目录下没有文件\033[39m');
     }
     console.log('选择文件或目录:');
+
     function file(i) {
         var filename = files[i];
         fs.stat(__dirname + '/' + filename, function (err, stat) {
@@ -13,17 +16,30 @@ fs.readdir(process.cwd(), function (err, files) {
             } else {
                 console.log('    ' + i + '    \033[90m' + filename + '\033[39m');
             }
-            i++;
-            if (i == files.length) {
-                console.log('');
-                process.stdout.write('    \033[33m选择:   \033[39m');
-                process.stdin.resume();
-                process.stdin.setEncoding('utf8');
+            if (++i == files.length) {
+                read();
             } else {
                 file(i);
             }
         });
     }
 
+    function read() {
+        console.log('');
+        stdout.write('    \033[33m选择:    \033[39m');
+        stdin.resume();
+        stdin.setEncoding('utf8');
+        stdin.on('data', option);
+    }
+
+    function option(data) {
+        var choice = Number(data);
+        if (files[choice]) {
+            stdin.pause();
+        } else {
+            stdout.write('    \033[31m重试:    \033[39m');
+        }
+    }
+    
     file(0);
 });
