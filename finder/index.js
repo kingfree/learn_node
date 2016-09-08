b11dd5ad5481454e079db77a649c742a0fafe662
Kingfree <anytjf@gmail.com>
2016-09-08 17:41:42 +0800

    支持目录

--- finder/index.js
+++ finder/index.js
@@ -7,10 +7,13 @@ fs.readdir(process.cwd(), function (err, files) {
         return console.log('    \033[31m 当前目录下没有文件\033[39m');
     }
     console.log('选择文件或目录:');
+    var stats = [];
 
     function file(i) {
         var filename = files[i];
-        fs.stat(__dirname + '/' + filename, function (err, stat) {
+        var path = __dirname + '/' + filename;
+        fs.stat(path, function (err, stat) {
+            stats[i] = stat;
             if (stat.isDirectory()) {
                 console.log('    ' + i + '    \033[36m' + filename + '/\033[39m');
             } else {
@@ -34,12 +37,28 @@ fs.readdir(process.cwd(), function (err, files) {
 
     function option(data) {
         var choice = Number(data);
-        if (files[choice]) {
+        var filename = files[choice];
+        if (filename) {
+            var path = __dirname + '/' + filename;
             stdin.pause();
+            if (stats[choice].isDirectory()) {
+                fs.readdir(path, function (err, files) {
+                    console.log('');
+                    console.log('    (' + files.length + ' 个文件)');
+                    files.forEach(function (file) {
+                        console.log('     - ' + file);
+                    });
+                });
+            } else {
+                fs.readFile(path, 'utf8', function (err, data) {
+                    console.log('');
+                    console.log('\033[90m' + data.replace(/(.*)/g, '    $1') + '\033[39m')
+                });
+            }
         } else {
             stdout.write('    \033[31m重试:    \033[39m');
         }
     }
-    
+
     file(0);
 });
