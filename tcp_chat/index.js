b37e10faa0f8805e237a6fbeff10d6544e5ee096
Kingfree <anytjf@gmail.com>
2016-09-09 11:21:50 +0800

    连接计数

--- tcp_chat/index.js
+++ tcp_chat/index.js
@@ -1,9 +1,17 @@
 var net = require('net');
 
+var count = 0;
+
 var server = net.createServer(function (conn) {
-   console.log('\033[90m    连接建立\033[39m');
+    conn.write('\n > 欢迎使用      \033[92mNode Chat\033[39m!' +
+        '\n > 当前在线用户: ' + count + ' 人  ' +
+        '\n > 输入你的昵称:      ');
+    count++;
+    conn.on('close', function () {
+        count--;
+    });
 });
 
 server.listen(3000, function () {
-   console.log('\033[96m    服务监听在: *:3000\033[39m');
+    console.log('\033[96m    服务监听在: *:3000\033[39m');
 });
