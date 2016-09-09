97bb2d02d699f7e18bd4d078718d97dedf6454ef
Kingfree <anytjf@gmail.com>
2016-09-09 11:34:52 +0800

    简单交流

--- tcp_chat/index.js
+++ tcp_chat/index.js
@@ -1,14 +1,43 @@
 var net = require('net');
 
 var count = 0;
+var users = {};
 
 var server = net.createServer(function (conn) {
-    conn.write('\n > 欢迎使用      \033[92mNode Chat\033[39m!' +
+    var nickname, i;
+
+    conn.setEncoding('utf8');
+
+    conn.write('\n > 欢迎使用    \033[92mNode Chat\033[39m!' +
         '\n > 当前在线用户: ' + count + ' 人  ' +
-        '\n > 输入你的昵称:      ');
+        '\n > 输入你的昵称:    ');
+
     count++;
+
+    conn.on('data', function (data) {
+        data = data.replace('\r\n', '');
+        if (!nickname) {
+            if (users[data]) {
+                conn.write('\033[93m 昵称已存在，换个别的吧:    \033[39m');
+            } else {
+                nickname = data;
+                users[nickname] = conn;
+                for (i in users) {
+                    users[i].write('\033[90m > ' + nickname + ' 上线了！   \033[39m\n');
+                }
+            }
+        } else {
+            for (i in users) {
+                if (i != nickname) {
+                    users[i].write('\033[96m > ' + nickname + ': \033[39m' + data + '\n');
+                }
+            }
+        }
+    });
+
     conn.on('close', function () {
         count--;
+        delete users[nickname];
     });
 });
 
