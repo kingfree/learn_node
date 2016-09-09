244b04228d0bcd5cde66d58e15a5d8661c1756cb
Kingfree <anytjf@gmail.com>
2016-09-09 17:27:09 +0800

    打印头部

--- webserver.js
+++ webserver.js
@@ -3,6 +3,9 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    res.end('<h1>Hello Node.js</h1>');
+    res.write('<h1>Hello Node.js</h1>');
+    setTimeout(function () {
+        res.end('<pre>' + JSON.stringify(req.headers, null, 2) + '</pre>');
+    }, 500);
 });
 server.listen(8080);
