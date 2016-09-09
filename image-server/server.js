30263330ab0a863904bb6b17b712102d36902e7a
Kingfree <anytjf@gmail.com>
2016-09-09 17:53:12 +0800

    简单的HTTP服务器——图片浏览

---
+++ image-server/server.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var fs = require('fs');
+const ROOT_DIR = __dirname;
+
+var server = http.createServer(function (req, res) {
+
+    function serve(path, type) {
+        res.writeHead(200, {
+            'Content-Type': type
+        });
+        fs.createReadStream(path).pipe(res);
+    }
+
+    if (req.method == 'GET' && req.url.substr(0, 7) == '/images' && req.url.substr(-4) == '.jpg') {
+        fs.stat(ROOT_DIR + req.url, function (err, stat) {
+            if (err || !stat.isFile()) {
+                res.writeHead(404);
+                res.end('File Not Found');
+            } else {
+                serve(ROOT_DIR + req.url, 'application/jpeg');
+            }
+        });
+    } else if (req.method == 'GET' && req.url == '/') {
+        serve(ROOT_DIR + '/index.html', 'text/html');
+    } else {
+        res.writeHead(404);
+        res.end('404 Not Found');
+    }
+
+});
+
+server.listen(3000);
