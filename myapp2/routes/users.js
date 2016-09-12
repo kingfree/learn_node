11ad7207853ab21a266ba0efe7a5cedb72e3b17e
Kingfree <anytjf@gmail.com>
2016-09-12 12:02:48 +0800

    基本路由

--- myapp2/routes/users.js
+++ myapp2/routes/users.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  res.send('SELECT');
+});
+
+router.post('/', function (req, res) {
+  res.send('INSERT');
+});
+
+router.put('/', function (req, res) {
+  res.send('UPDATE');
+});
+
+router.delete('/', function (req, res) {
+  res.send('DELETE');
 });
 
 module.exports = router;
