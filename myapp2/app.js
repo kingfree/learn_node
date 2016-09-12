243d30b5e3cd5d8a6d9c3067adea1bfdd188bce5
Kingfree <anytjf@gmail.com>
2016-09-12 16:44:26 +0800

    使用中间件

--- myapp2/app.js
+++ myapp2/app.js
@@ -18,15 +18,40 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  next();
+});
+
+app.use('/user/:id', function (req, res, next) {
+  console.log('Request:', req.method);
+  next();
+});
+
+app.get('/user/:id', function (req, res, next) {
+  console.log('USER ' + req.params.id);
+  if (req.params.id == 0) {
+    next('route');
+  } else {
+    next();
+  }
+}, function (req, res, next) {
+  res.send('User Info');
+});
+
+app.get('/user/:id', function (req, res, next) {
+  res.end('ID: ' + req.params.id);
+});
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -37,7 +62,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -48,7 +73,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
