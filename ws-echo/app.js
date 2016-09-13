4c80e51f7aa7926f732a4239680541cde1632cc5
Kingfree <anytjf@gmail.com>
2016-09-13 13:56:28 +0800

    光标移动通信利用WebSocket

--- ws-echo/app.js
+++ ws-echo/app.js
@@ -14,9 +14,48 @@ app.listen(3000);
 
 var server = ws.listen(3001);
 
+var positions = {};
+var total = 0;
+var clients = [];
+
 server.on('connection', function (socket) {
+  socket.id = ++total;
+  socket.send(JSON.stringify(positions));
+  clients[socket.id] = socket;
+
   socket.on('message', function (msg) {
     console.log(msg);
-    socket.send('pong');
+    if (msg == 'ping') {
+      socket.send('pong');
+    } else {
+      try {
+        positions[socket.id] = JSON.parse(msg);
+        broadcast(JSON.stringify({
+          type: 'position',
+          pos: positions[socket.id],
+          id: socket.id
+        }));
+      } catch (e) {
+        console.error(e);
+      }
+    }
   });
+
+  socket.on('close', function () {
+    delete positions[socket.id];
+    delete clients[socket.id];
+    broadcast(JSON.stringify({
+      type: 'disconnect',
+      id: socket.id
+    }));
+  });
+
+  function broadcast(msg) {
+    for (var id in clients) {
+      var client = clients[id];
+      if (client && client.id != socket.id) {
+        client.send(msg);
+      }
+    }
+  }
 });
