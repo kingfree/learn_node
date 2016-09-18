2124f0a7ede03f5ca3ab374ec38e819d91aedd80
Kingfree <anytjf@live.com>
2016-09-18 16:41:14 +0800

    实现登录

--- user-auth/app.js
+++ user-auth/app.js
@@ -7,10 +7,12 @@ var bodyParser = require('body-parser');
 var http = require("http");
 var mongodb = require("mongodb");
 var passwordHash = require("password-hash");
+var session = require('express-session');
 
 var app = express();
 var server = new mongodb.Server('127.0.0.1', 27017);
 var db = new mongodb.Db('user-auth', server);
+var port = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,20 +23,55 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'Pripara'}));
 
 db.open(function (err, client) {
   if (err) throw err;
   console.log('已连接到数据库');
 
   app.users = client.collection('users');
+  client.ensureIndex('users', 'username', function (err) {
+    if (err) throw err;
+    client.ensureIndex('users', 'nickname', function (err) {
+      if (err) throw err;
+      console.log('已建立索引');
+      app.listen(port, function () {
+        console.log('服务监听在 *:' + port);
+      });
+    });
+  });
+});
+
+app.use(function (req, res, next) {
+  if (req.session.loggedIn) {
+    app.users.findOne({_id: new mongodb.ObjectId(req.session.loggedIn)}, function (err, result) {
+      if (err) next(err);
+      res.locals.authenticated = true;
+      res.locals.me = result;
+      next();
+    });
+  } else {
+    res.locals.authenticated = false;
+    next();
+  }
 });
 
 app.get('/', function (req, res, next) {
-  res.render('index', {title: '主页', authenticated: false});
+  res.render('index', {title: '主页'});
 });
 
 app.get('/login', function (req, res, next) {
   res.render('login', {title: '登录'});
+});
+
+app.post('/login', function (req, res, next) {
+  app.users.findOne({username: req.body.username}, function (err, result) {
+    if (err) return next(err);
+    if (!result) return res.send('用户不存在！');
+    if (!checkPassword(req.body.password, result.password)) return res.send('密码错误！');
+    req.session.loggedIn = result._id.toString();
+    res.redirect('/');
+  });
 });
 
 app.get('/login/:username', function (req, res, next) {
@@ -58,16 +95,17 @@ app.post('/register', function (req, res, next) {
   });
 });
 
+app.get('/logout', function (req, res) {
+  req.session.loggedIn = null;
+  res.redirect('/');
+});
+
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: err
   });
-});
-
-app.listen(3000, function () {
-  console.log('服务监听在 *:3000');
 });
 
 function generatePassword(password) {
